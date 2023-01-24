e1b1af79f960a3b52ca1ccd5d074d05d72c93567
Mohamed Fazrin <mfmfazrin1986@gmail.com>
2023-01-24 11:39:35 +0300

    Initial commit

---
+++ ImprovedDictionary/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World!");
+
+var myExistingClass = new MyClass();
+
+var dictionary = new Dictionary<Guid, MyClass>()
+{
+    { myExistingClass.Id, myExistingClass }
+};
+
+var myNewClass = new MyClass();
+
+/*
+if(!dictionary.ContainsKey(myNewClass.Id))
+{
+    dictionary[myNewClass.Id] = myNewClass;
+    // dictionary.Add(myNewClass.Id, myNewClass);
+}
+
+if(dictionary.TryGetValue(myNewClass.Id, out var val))
+{
+    val.Id = Guid.NewGuid();
+}
+*/
+
+ref var valOrNew = ref CollectionsMarshal.GetValueRefOrAddDefault(dictionary, myNewClass.Id, out var existed);
+
+if(!existed)
+{
+    valOrNew = myNewClass;
+}
+
+
+if(!Unsafe.AreSame(ref valOrNew, ref myNewClass))
+{
+    valOrNew.Id = Guid.NewGuid();
+}
+
+if(!Unsafe.IsNullRef(ref valOrNew))
+{
+    valOrNew.Id = Guid.NewGuid();
+}
+
+
+public class MyClass
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+}
